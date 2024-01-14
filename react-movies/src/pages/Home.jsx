d2a4200d272c843e859198ea7121b4805222679f
Maite Arakaki <maite.arakaki@gmail.com>
2024-01-14 18:51:01 -0500

    creado el componente buscador

--- react-movies/src/pages/Home.jsx
+++ react-movies/src/pages/Home.jsx
@@ -1,6 +1,14 @@
+import { Box } from "@mui/material"
+import { Buscador } from "../components/buscador/Buscador"
+
 function Home (){
     return(
-        <h1>home</h1>
+        <>
+            <h1>home</h1>
+            <Box sx={{width:"80vw", margin:'0 auto'}}>
+                <Buscador/>
+            </Box>
+        </>
     )
 }
 
