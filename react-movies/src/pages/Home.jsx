f4ba5a84fcdec6e6ffb1e44d051176efc58fc192
Maite Arakaki <maite.arakaki@gmail.com>
2024-01-14 19:02:39 -0500

    switched to dark theme

--- react-movies/src/pages/Home.jsx
+++ react-movies/src/pages/Home.jsx
@@ -1,13 +1,22 @@
-import { Box } from "@mui/material"
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import { Buscador } from "../components/buscador/Buscador"
 
 function Home (){
+    const darkTheme = createTheme({
+        palette: {
+          mode: 'dark',
+        },
+      });
+
     return(
         <>
-            <h1>home</h1>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
             <Box sx={{width:"80vw", margin:'0 auto'}}>
                 <Buscador/>
             </Box>
+            
+        </ThemeProvider>
         </>
     )
 }
