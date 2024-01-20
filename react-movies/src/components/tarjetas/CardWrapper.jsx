c73656ea36e05296ed089157b15dcf12c02f09c5
Maite Arakaki <maite.arakaki@gmail.com>
2024-01-20 17:22:54 -0500

    eliminados export innecesarios y modificado loading ln 37

--- react-movies/src/components/tarjetas/CardWrapper.jsx
+++ react-movies/src/components/tarjetas/CardWrapper.jsx
@@ -1,23 +1,20 @@
 import React, { useState } from 'react'
 import Tarjeta from '../tarjeta/Tarjeta'
 import movies from '../../assets/movies.json'
-import style from './CardWrapper.module.css'
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 const CardWrapper = () => {
-  const [items, setItems] = useState(movies.slice(0, 3)); // Initial set of items
+  const [items, setItems] = useState(movies.slice(0, 3));
   const [hasMore, setHasMore] = useState(true);
 
   const fetchMoreData = () => {
-    // Simulate loading more data, you can replace this with your actual data fetching logic
     setTimeout(() => {
       const nextItems = movies.slice(items.length, items.length + 3);
       setItems([...items, ...nextItems]);
 
-      // Update hasMore based on your data length or any other condition
       setHasMore(items.length < movies.length);
-    }, 500); // Adjust the timeout as needed
+    }, 500); 
   };
 
   return (
@@ -37,7 +34,7 @@ const CardWrapper = () => {
     dataLength={items.length}
     next={fetchMoreData}
     hasMore={hasMore}
-    loader={<h4>Loading...</h4>}
+    loader={<h4>Cargando...</h4>}
     >
     </InfiniteScroll>
     </div>
