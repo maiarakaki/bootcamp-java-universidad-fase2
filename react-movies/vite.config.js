05a361db7de04860686d7a172a38be15a4ae34d5
Maite Arakaki <maite.arakaki@gmail.com>
2024-01-20 18:20:08 -0500

    reverted base in vite.config.js

--- react-movies/vite.config.js
+++ react-movies/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://maiarakaki.github.io/bootcamp-java-universidad-fase2/'
 })
